5e8b0720050aef7c899c6d6c666477b18b549f26
Dwight Fernandez <ddfernandez@student.apc.edu.ph>
2026-01-29 12:10:23 +0000

    changes

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
  
 import App from './App.vue'
-import FoodItem from '.src/components/FoodItem.vue'
- 
+import FoodItem from './components/FoodItem.vue'
+
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.mount('#app')
