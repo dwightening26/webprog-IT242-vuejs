19aa9285936fa5753a9098a25933d6fc862dfeda
Dwight Fernandez <ddfernandez@student.apc.edu.ph>
2026-01-29 10:11:04 +0000

    editing app and main

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -7,17 +7,3 @@ const app = createApp(App)
 app.component('food-item', FoodItem)
 app.mount('#app')
  
-App.vue
- 
- 
-<template>
-  <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-</template>
- 
-<script></script>
- 
-<style></style>
- 
