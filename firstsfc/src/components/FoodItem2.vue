b2e4ec6371cf1c061c72656b6049318ce304470b
Dwight Fernandez <ddfernandez@student.apc.edu.ph>
2026-02-02 13:37:43 +0000

    adding second component

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Grapes and Strawberies',
+      message: 'I like Grapes and Strawberies'
+    }
+  }
+}
+</script>
+
+<style>
+.box {
+  border: 1px dashed black;
+  background-color: rgb(201, 98, 106);
+  padding: 20px;
+  flex: 1;
+}
+</style>
