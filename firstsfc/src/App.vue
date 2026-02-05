5b931843fc9b34aedacf9f17cf582f2bd731ddc3
Dwight Fernandez <ddfernandez@student.apc.edu.ph>
2026-02-05 10:55:01 +0000

    asddsas

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,52 +1,21 @@
-<template>
-  <div>
-    <h1>Fruits</h1>
-
-    <div class="container">
-      <FoodItem />
-      <FoodItem2 />
-    </div>
-
-    <hr />
-
-    <PersonalProfile />
-
-    <hr />
-
-    <ul>
-      <li v-for="instrument in instruments" :key="instrument.id">
-        {{ instrument.name }}
-      </li>
-    </ul>
-  </div>
-</template>
-
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
 
-import FoodItem from './components/FoodItem.vue'
-import FoodItem2 from './components/FoodItem2.vue'
-import PersonalProfile from './components/PersonalProfile.vue'
-
 const instruments = ref([])
 
-onMounted(async () => {
-  const { data, error } = await supabase.from('instruments').select()
-  if (!error) {
-    instruments.value = data
-  }
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
 })
 </script>
 
-<style>
-.container {
-  display: flex;
-  gap: 20px;
-  align-items: stretch;
-}
-
-hr {
-  margin: 40px 0;
-}
-</style>
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
