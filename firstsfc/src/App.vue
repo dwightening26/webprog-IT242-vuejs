1aac873e9485706d5bb48af51f7bab750e10f860
Dwight Fernandez <ddfernandez@student.apc.edu.ph>
2026-02-07 19:06:15 +0800

    Task 03 Add view and insert comment/visitor feedback functionality to your website

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,35 +1,13 @@
 <template>
-  <div>
-    <h1>Fruits</h1>
-    <div class="container">
-      <food-item />
-      <food-item2 />
-    </div>
+  <h1>Food</h1>
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
 
-    <hr />
-
-    <PersonalProfile />
-  </div>
 </template>
 
-<script>
-import PersonalProfile from './components/PersonalProfile.vue'
-
-export default {
-  components: {
-    PersonalProfile
-  }
-}
-</script>
-
-<style>
-.container {
-  display: flex;
-  gap: 20px;
-  align-items: stretch;
-}
-
-hr {
-  margin: 40px 0;
-}
-</style>
+<script></script>
